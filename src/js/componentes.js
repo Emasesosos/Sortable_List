37fc01a80d292b8b789ef457ddd5aed281a2bdda
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-06-22 20:47:33 -0500

    Scramble List Items

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -25,23 +25,33 @@ let dragStartIndex;
 // Insert list items into DOM
 const createList = () => {
     [...richestPeople]
-    .forEach((person, index) => {
-        const listItem = document.createElement('li');
-        listItem.setAttribute('data-index', index);
-        listItem.innerHTML = `
+    .map((a) => {
+            return { value: a, sort: Math.random() };
+        })
+        .sort((a, b) => {
+            return a.sort - b.sort
+        })
+        .map((a) => {
+            return a.value
+        })
+        .forEach((person, index) => {
+            // console.log(person);
+            const listItem = document.createElement('li');
+            listItem.setAttribute('data-index', index);
+            listItem.innerHTML = `
             <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
                 <p class="person-name">${person}</p>
                 <i class="fas fa-grip-lines"></i>
             </div>
         `;
-        listItems.push(listItem);
-        draggable_list.appendChild(listItem);
-    });
+            listItems.push(listItem);
+            draggable_list.appendChild(listItem);
+        });
 };
 /* ************************************************************ */
 const events = () => {
-    console.log('Event Listeners');
+    // console.log('Event Listeners');
 };
 /* ************************************************************ */
 const init = () => {
