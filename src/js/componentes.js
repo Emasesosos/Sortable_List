1402cb6f0fd2b7c8c299d69a69012b2030eacc9d
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-06-22 20:35:37 -0500

    Insert List Items Into DOM

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,44 @@
 /* CSS */
 import './../css/componentes.css';
 /* DOM */
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richestPeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffett',
+    'Bernard Arnault',
+    'Carlos Slim Helu',
+    'Amancio Ortega',
+    'Larry Ellison',
+    'Mark Zuckerberg',
+    'Michael Bloomberg',
+    'Larry Page'
+];
+
+// Store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+// Insert list items into DOM
+const createList = () => {
+    [...richestPeople]
+    .forEach((person, index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>
+        `;
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+    });
+};
 /* ************************************************************ */
 const events = () => {
     console.log('Event Listeners');
@@ -8,6 +46,7 @@ const events = () => {
 /* ************************************************************ */
 const init = () => {
     console.log('Sortable List');
+    createList();
     events();
 };
 /* ************************************************************ */
